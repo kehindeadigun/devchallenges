942328598a2d910dd0ec6ae13bc6c59264d2a935
Kehinde Adigun <kehinde@getclue.com>
2026-03-26 03:41:08 +0100

    Create faq js challenge

---
+++ JS Challenges/FAQ Challenge/main.js
@@ -0,0 +1,15 @@
+const data = document.querySelectorAll(".data__toggle");
+const list = document.querySelectorAll(".options li");
+
+function handleFAQClick ({target}) {
+    const dataElem = target.closest(".data");
+    desc = dataElem.querySelector(".data__description");
+    if (!desc) {return}
+    dataElem.classList.toggle("active");
+    dataElem.classList.toggle("padded");
+    target.classList.toggle("rotated");
+    desc.classList.toggle("hidden");
+}
+
+
+data.forEach(ele => ele.addEventListener("click", handleFAQClick))
